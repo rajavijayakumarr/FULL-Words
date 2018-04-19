bde1ad48f4af7faf9a9d5529130127d560f6aa77
User <user@full157-air.local>
2018-04-19 13:22:00 +0530

    added the navigation controller and the tab bar controller and yet to push the username onto the tabor dashboard

--- FULL Words/ViewController.swift
+++ FULL Words/ViewController.swift
@@ -16,64 +16,82 @@ let ADAPTIVEU_CLIENT_SECRET = "iOsdmGZj-8Tydm0sJ8l6N6dWCZ_e0uZQ5LTy2KfT"
 let ADAPTIVEU_REDIRECT_URL = "com.FULL.FULL-Words://"
 let ADAPTIVEU_CODE_URL = "https://access.anywhereworks.com/o/oauth2/auth"
 let ADAPTIVEU_TOKEN_URL = "https://access.anywhereworks.com/o/oauth2/v1/token"
+let ADAPTIVEU_SCOPE_URL = "https://api.anywhereworks.com/api/v1/user/me"
 
 let ADAPTIVIEWU_REFRESH_TOKEN = "ADAPTIVIEWU_REFRESH_TOKEN"
 let ADAPTIVIEWU_ACCESS_TOKEN = "ADAPTIVIEWU_ACCESS_TOKEN"
+let ADAPTIVIEWU_TOKEN_TYPE = "ADAPTIVIEWU_TOKEN_TYPE"
+
 
 let UNIQUE_STATE_TOKEN_VERIFY = "validated_token"
-
 
 class ViewController: UIViewController, UIScrollViewDelegate {
     
     var authenticationSession: SFAuthenticationSession? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
 
-        let url = ADAPTIVEU_CODE_URL+"?response_type=code&client_id="+ADAPTIVEU_CLIENT_ID+"&access_type=offline&scope=awapis.identity&redirect_uri="+ADAPTIVEU_REDIRECT_URL+"&approval_prompt=force&state="+UNIQUE_STATE_TOKEN_VERIFY
+        let url = ADAPTIVEU_CODE_URL+"?response_type=code&client_id="+ADAPTIVEU_CLIENT_ID+"&access_type=offline&scope=awapis.identity&redirect_uri="+ADAPTIVEU_REDIRECT_URL+"&approval_prompt=force"
         authenticationSession = SFAuthenticationSession(url: URL(string: url)!, callbackURLScheme: ADAPTIVEU_REDIRECT_URL, completionHandler: { (receivedURL, error) in
+            
             let authenticationCode = self.getTheAuthenticationCode(from: receivedURL)
             print(receivedURL?.absoluteString ?? "nothing")
             print(authenticationCode)
             
-            var request = URLRequest(url: URL(string: ADAPTIVEU_TOKEN_URL)!)
-            request.setValue("https://access.anywhereworks.com", forHTTPHeaderField: "Host")
-            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+            var requestForGettingToken = URLRequest(url: URL(string: ADAPTIVEU_TOKEN_URL)!)
 
             var data:Data = "code=\(authenticationCode)".data(using: .utf8)!
             data.append("&client_id=\(ADAPTIVEU_CLIENT_ID)".data(using: .utf8)!)
             data.append("&client_secret=\(ADAPTIVEU_CLIENT_SECRET)".data(using: .utf8)!)
             data.append("&redirect_uri=\(ADAPTIVEU_REDIRECT_URL)".data(using: .utf8)!)
             data.append("&grant_type=authorization_code".data(using: .utf8)!)
-            request.httpBody = data
-            request.httpMethod = "POST"
+            requestForGettingToken.httpBody = data
+            requestForGettingToken.httpMethod = "POST"
             
             print(String(data: data, encoding: String.Encoding(rawValue: String.Encoding.utf8.rawValue)) as Any)
 
-
-            Alamofire.request(URL(string: ADAPTIVEU_TOKEN_URL)!).responseJSON { (responseData) in
-                print(NSString(data: responseData.data! , encoding: String.Encoding.utf8.rawValue) as Any)
+            Alamofire.request(requestForGettingToken).responseJSON { (responseData) in
+            if responseData.error == nil{
+                var dataContainingTokens = JSON(responseData.data!)
+                let accessToken = dataContainingTokens["access_token"].stringValue
+                let refreshToken = dataContainingTokens["refresh_token"].stringValue
+                let tokenType = dataContainingTokens["token_type"].stringValue
+                    
+                    print("****************************************************************************************")
+                    print("accessToken: \(accessToken)\nrefreshToken: \(refreshToken)")
+                UserDefaults.standard.set(refreshToken, forKey: ADAPTIVIEWU_REFRESH_TOKEN)
+                UserDefaults.standard.set(accessToken, forKey: ADAPTIVIEWU_ACCESS_TOKEN)
+                UserDefaults.standard.set(tokenType, forKey: ADAPTIVIEWU_TOKEN_TYPE)
+                    
+                var requestForGettingUserDate = URLRequest(url: URL(string: ADAPTIVEU_SCOPE_URL)!)
+                requestForGettingUserDate.setValue(tokenType + " " + accessToken, forHTTPHeaderField: "Authorization")
+                requestForGettingToken.httpMethod = "GET"
+                    
+                Alamofire.request(requestForGettingUserDate).responseJSON { (responseData) in
+                    if responseData.error == nil {
+                    var dataContainingUserDetails = JSON(responseData.data!)
+                    print("****************************************************************************************")
+                    let firstName = dataContainingUserDetails["data"]["user"]["firstName"]
+                    let lastName = dataContainingUserDetails["data"]["user"]["lastName"]
+                    let emailId = dataContainingUserDetails["data"]["user"]["login"]
+                    print("****************************************************************************************")
+                    print("firstname: \(firstName)\nsecondname: \(lastName)\nemailId: \(emailId)")
+                        
+                        
+                        
+                }
+                }
+            }
             }
             
-//            let headers = ["Host":"https://access.anywhereworks.com",
-//                           "Content-Type":"application/x-www-form-urlencoded"]
-//            let parameters = ["grant_type": "authorization_code",
-//                              "client_id": ADAPTIVEU_CLIENT_ID,
-//                              "redirect_uri": ADAPTIVEU_REDIRECT_URL,
-//                              "code": String(authenticationCode)]
-//
-//            Alamofire.request(URL(string: ADAPTIVEU_TOKEN_URL)!, method:.post, parameters:parameters, headers:headers).validate(contentType: ["application/json"]).responseJSON { response in
-//                switch response.result {
-//                case .success:
-//                    debugPrint(response)
-//
-//                case .failure(let error):
-//                    print(error)
-//                }
-//
-//            }
-
+            
+            
+            
+            
+            
+            
             
         })
     }
@@ -83,15 +101,11 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     }
     
     func getTheAuthenticationCode(from url:URL?) -> String {
-        guard let url = url else {
-            return "no url found"
-        }
-        let stringUrl:String! = url.absoluteString
+
+        let stringUrl:String! = url?.absoluteString
         let index = stringUrl?.index(after: stringUrl?.index(of: "=") ?? (stringUrl?.startIndex)!)
         let substring = stringUrl[index!...]
-        let anotherIndex = substring.index(before: substring.index(of: "&")!)
-        let substringOfUrl = stringUrl[index!...anotherIndex]
-        return String(substringOfUrl)
+        return String(substring)
     }
     
 }
---
+++ FULL Words/dashBoardViewController.swift
@@ -0,0 +1,28 @@
+//
+//  dashBoardViewController.swift
+//  FULL Words
+//
+//  Created by User on 19/04/18.
+//  Copyright © 2018 FULL. All rights reserved.
+//
+
+import UIKit
+
+class dashBoardViewController: UIViewController {
+    
+    var userName: String?
+    var emailId: String?
+
+    @IBOutlet weak var userNameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        userNameLabel.text = userName
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
