8a0228ef08a08e2dfb4509a7b5d1a009d86496b1
User <user@full157-air.full.creative>
2018-06-01 11:12:51 +0530

    changed the tabbar menu items images and removed the text wrote custom class for keeping it in the middle of the view

--- FULL Words/TabBarControllersAllTabs/UserPageTabController.swift
+++ FULL Words/TabBarControllersAllTabs/UserPageTabController.swift
@@ -42,3 +42,32 @@ class UserPageTabController: UITabBarController {
     }
 
 }
+
+
+// this class is just to make the images in the tabbar be in the middle of the view when the text is not present in the tabbar controller
+class MainTabBar: UITabBar {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // iOS 11: puts the titles to the right of image for horizontal size class regular. Only want offset when compact.
+        // iOS 9 & 10: always puts titles under the image. Always want offset.
+        var verticalOffset: CGFloat = 6.0
+        
+        if #available(iOS 11.0, *), traitCollection.horizontalSizeClass == .regular {
+            verticalOffset = 0.0
+        }
+        
+        let imageInset = UIEdgeInsets(
+            top: verticalOffset,
+            left: 0.0,
+            bottom: -verticalOffset,
+            right: 0.0
+        )
+        
+        for tabBarItem in items ?? [] {
+            tabBarItem.title = ""
+            tabBarItem.imageInsets = imageInset
+        }
+    }
+}
